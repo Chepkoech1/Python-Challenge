2054034dd3ee1656b53dcc97783e09154d160804
Sandra Chepkoech <sandrachepkoech60@gmail.com>
2026-02-19 18:41:53 +0300

    listed variables in python

--- Day2.py
+++ Day2.py
@@ -1,1 +1,14 @@
 #Variables in python
+first_name = 'Sandra'
+last_name = 'Chepkoech'
+country = 'Kenya'
+city = 'Nairobi'
+age = 24
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Sandra,
+   'lastname':'Chepkoech,
+   'country':'Kenya',
+   'city':'Nairobi'
+   }
