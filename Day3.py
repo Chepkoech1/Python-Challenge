ab967e8476badf733f8753cdc1059548e91e7bd4
Sandra Chepkoech <sandrachepkoech60@gmail.com>
2026-02-26 15:42:08 +0300

    performed division

--- Day3.py
+++ Day3.py
@@ -4,3 +4,8 @@
 print('Addition: ', 1 + 2)
 print('Subtraction: ', 2 - 1)
 print('Multiplication: ', 2 * 3)
+
+# Division in python gives floating number
+print('Division: ', 4 / 2)
+print('Division: ', 6 / 2)
+print('Division: ', 7 / 2)
