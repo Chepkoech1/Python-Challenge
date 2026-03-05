9220db9af193927e3db35b691082765ea36232ac
Sandra Chepkoech <sandrachepkoech60@gmail.com>
2026-03-05 22:07:48 +0300

    strings in python

--- Day4.py
+++ Day4.py
@@ -1,5 +1,21 @@
+# strings
+print("Hello")
+print('Hello')
+
+# quotes inside quotes
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
 # F-strings
 name = "Alice"
 age = 42
 print(f"My name is {name} and I am {age} years old.") 
 # Output: My name is Alice and I am 42 years old.
+
+# multiline strings/three double quotes
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
